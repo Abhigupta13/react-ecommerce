4f7408d6a9cc3a380c6a7f1a12db6245a2ea4d4e
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-11-17 23:07:05 +0530

    Added routing login/signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+
+import {
+  createBrowserRouter,
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+  },
+  {
+    path: '/login',
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-     <Home/>
-    </div>
+    <Router>
+    <Routes>
+    <Route exact path= '/' element={ <Home/>} />
+    <Route exact path= '/login' element={ <LoginPage/>} />
+    <Route exact path= '/signup' element={ <SignupPage/>} />
+    </Routes>
+    </Router>
   );
 }
 
