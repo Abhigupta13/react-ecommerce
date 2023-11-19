8bf1be481356b8f25aa4af550bf797e2ac9bd891
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-11-19 12:51:22 +0530

    Login/SignUp Added & Cart added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import CartPage from './pages/CartPage';
-import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
-import ProductDetailPage from './pages/ProductDetailPage';
 import SignupPage from './pages/SignupPage';
+import Cart from './features/cart/Cart';
+import CartPage from './pages/CartPage';
+import Checkout from './pages/Checkout';
+import ProductDetailPage from './pages/ProductDetailPage';
+import Protected from './features/auth/components/Protected';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectLoggedInUser } from './features/auth/authSlice';
+import { fetchItemsByUserIdAsync } from './features/cart/cartSlice';
 import {BrowserRouter as Router,Route,Routes,} from 'react-router-dom';
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser);
+
+  useEffect(()=>{
+    if(user){
+      dispatch(fetchItemsByUserIdAsync(user.id))
+    }
+  },[dispatch, user])
   return (
+
     <Router><Routes>
     <Route exact path= '/' element={ <Home/>} />
-    <Route exact path= '/login' element={ <LoginPage/>} />
+    <Route exact path= '/login' element={ <LoginPage/> } />
     <Route exact path= '/signup' element={ <SignupPage/>} />
-    <Route exact path= '/cart' element={ <CartPage/>} />
-    <Route exact path= '/checkout' element={ <Checkout/>} />
-    <Route exact path= '/product-detail/:id' element={ <ProductDetailPage/>} />
+    <Route exact path= '/cart' element={<> <Protected/> <CartPage/></>} />
+    <Route exact path= '/checkout' element={<> <Protected/> <Checkout/></>} />
+    <Route exact path= '/product-detail/:id' element={<> <Protected/> <ProductDetailPage/></>} />
     </Routes></Router>
   );
 }
