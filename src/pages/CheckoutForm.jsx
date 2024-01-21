b9f2f2bc04ed4cf174103fd9d42edcbea59e4862
Abhishek Kumar Gupta <100848632+Abhigupta13@users.noreply.github.com>
2024-01-21 17:22:47 +0530

    Update CheckoutForm.jsx

--- src/pages/CheckoutForm.jsx
+++ src/pages/CheckoutForm.jsx
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `https://mern-ecommerce-u3eu.onrender.com/order-success${currentOrder.id}`,
+        return_url: `https://mern-ecommerce-u3eu.onrender.com/order-success/${currentOrder.id}`,
       },
     });
 
